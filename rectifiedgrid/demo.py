fd89a70a8f8e60727b113607d4feda97b18e1615
Stefano Menegon <ste.menegon@gmail.com>
2022-04-21 23:24:08 +0200

    updated tests and fixed bugs

--- rectifiedgrid/demo.py
+++ rectifiedgrid/demo.py
@@ -1,9 +1,11 @@
-
-
 import numpy as np
+import geopandas as gpd
 import rectifiedgrid as rg
 from affine import Affine
 from shapely import geometry
+import xarray
+from rioxarray.rioxarray import affine_to_coords
+from rasterio.enums import Resampling
 
 
 epsg = 3035
@@ -25,7 +27,22 @@ arr2[4, 4] = 1
 arr3 = np.zeros((9, 9), np.float64)
 arr3[0, 0] = 1
 
+# now proj complies the axes order
 lstring = geometry.LineString(((12.0, 36.0), (13.0, 37.0)))
+# lstring = geometry.LineString(((36.0, 12.0), (37.0, 13.0)))
+df_lstring = gpd.GeoDataFrame(geometry=[lstring], crs="epsg:4326")
+df_lstring['value'] = 1
+
+def array_to_rg(_array, epsg, gtransform):
+    coords = affine_to_coords(gtransform, _array.shape[1], _array.shape[0])
+    raster = (
+        xarray.DataArray(_array, coords=coords)
+            .rio.write_nodata(np.nan)
+            .rio.write_crs(rg.parse_projection(epsg))
+            .rio.write_transform(gtransform)
+            .rio.write_coordinate_system()
+    )
+    return raster
 
 
 def get_demo_data(name='default'):
@@ -33,15 +50,13 @@ def get_demo_data(name='default'):
     """
     raster = None
     if name == 'default':
-        raster = rg.RectifiedGrid(arr1, rg.parse_projection(epsg), gtransform)
+        raster = array_to_rg(arr1, epsg, gtransform)
     elif name == 'rg9x9':
-        raster = rg.RectifiedGrid(arr2, rg.parse_projection(epsg), gtransform)
+        raster = array_to_rg(arr2, epsg, gtransform)
     elif name == 'line3035':
-        from_srs = 4326
-        to_srs = 3035
-        features = [(rg.transform(lstring, from_srs, to_srs), 1)]
+        features = df_lstring.to_crs("epsg:3035")[['geometry', 'value']].values.tolist()
         raster = rg.read_features(features, 10000, 3035, eea=True)
     elif name == 'line4326':
-        features = [(lstring, 1)]
+        features = df_lstring[['geometry', 'value']].values.tolist()
         raster = rg.read_features(features, 0.1, 4326)
     return raster.copy()
