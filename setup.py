6914afbaf3718874caa03969d6a23c52d4e12f20
Stefano Menegon <ste.menegon@gmail.com>
2022-04-27 11:10:15 +0200

    set new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="rectifiedgrid",  # or RectifiedGrid
-    version="2.0.2",
+    version="2.0.3",
     author="Stefano Menegon",
     author_email="ste.menegon@gmail.com",
     description="RectifiedGrid is a python module to deal with rectified grid.",
